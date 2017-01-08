6d285a64dd92ca728dea4ff5e531455f1b146114
videet <videetssinghai@gmail.com>
2017-01-09 00:10:56 +0530

    added filters

--- posts/api/views.py
+++ posts/api/views.py
@@ -5,6 +5,11 @@ from rest_framework.permissions import (
 AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 )
 from .permissions import IsOwnerOrReadOnly
+from django.db.models import Q
+from rest_framework.filters import (
+    SearchFilter,
+OrderingFilter
+)
 
 class PostCreateAPIView(CreateAPIView):
     queryset = Post.objects.all()
@@ -16,8 +21,22 @@ class PostCreateAPIView(CreateAPIView):
 
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    filter_backends = [SearchFilter]
+    search_fields = [ 'title' , 'content','user__first_name',]
+
+    def get_queryset(self,*args,**kwargs):
+        query = self.request.GET.get("q")
+        queryset_list =  Post.objects.all()
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+        return queryset_list
+
 
 
 class PostDetailAPIView(RetrieveAPIView):
