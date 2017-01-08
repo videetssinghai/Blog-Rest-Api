8262a6c39dc97817876c37472cdc4deecb5ccebb
videet <videetssinghai@gmail.com>
2017-01-08 14:41:50 +0530

    serializers partially done

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -2,10 +2,22 @@ from rest_framework.serializers import ModelSerializer
 from posts.models import Post
 
 
-class PostSerializer(ModelSerializer):
+class PostListSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = [
+            'title',
+            'slug',
+            'content',
+            'publish',
+        ]
+
+
+class PostDetailsSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'id',
             'title',
             'slug',
             'content',
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,7 +1,14 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from posts.models import Post
-from .serializers import PostSerializer
+from .serializers import PostListSerializer,PostDetailsSerializer
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostListSerializer
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailsSerializer
+    lookup_field = 'slug'
+
